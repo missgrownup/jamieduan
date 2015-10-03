facb50223b0dcb4561f5d63bb0e41f99c8e4d63f
Jamie <jamie@tundra.com.au>
2015-10-03 22:22:42 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Jamie Duan | Front End Developer, Melbourne</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<link href="assets/styles/main.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="cover">
+		</div>
+		<div id="page" class="fixed-top">
+			<?php include './src/components/header.html'; ?>
+			<?php include './src/components/about.html'; ?>
+			<?php include './src/components/skills.html'; ?>
+			<?php include './src/components/projects.html'; ?>
+			<?php include './src/components/travel.html'; ?>
+			<?php include './src/components/contact.html'; ?>
+			<?php include './src/components/footer.html'; ?>
+		</div>
+	<script src="assets/scripts/main.js" type="text/javascript"></script>
+	</body>
+</html>
---
+++ src/gulpfile.js
@@ -0,0 +1,81 @@
+(function() {
+
+	'use strict';
+
+	var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		gutil = require('gulp-util'),
+		rimraf = require('gulp-rimraf'),
+		concat = require('gulp-concat'),
+		uglify = require('gulp-uglify'),
+		browserify = require('gulp-browserify'),
+		bourbon = require('node-bourbon'),
+		neat = require('node-neat'),
+		source = './',
+		destination = '../assets/';
+
+	function errorHandler(error) {
+		gutil.beep();
+		gutil.log(
+			gutil.colors.bgGreen(error.plug),
+			gutil.colors.bgRed(gutil.colors.white(error.message.replace(/\r?\n|\r/g, '')))
+		);
+	}
+
+	gulp.task('clean', function() {
+		return gulp.src([
+				destination + 'styles',
+				destination + 'images',
+				destination + 'scripts',
+				destination + 'fonts'
+			], {read: false})
+			.pipe(rimraf({force: true}));
+	});
+
+	gulp.task('styles', function() {
+		return gulp.src(source + 'styles/main.scss')
+			.pipe(sass({
+				outputStyle: 'compressed',
+				includePaths: [source + 'styles'].concat(bourbon.includePaths, neat.includePaths),
+				errLogToConsole: true
+			}))
+			.pipe(gulp.dest(destination + 'styles'))
+			.on('error', errorHandler);
+	});
+
+	gulp.task('images', function() {
+		return gulp.src(source + 'images/**/*')
+			.pipe(gulp.dest(destination + 'scripts'))
+			.on('error', errorHandler);
+	});
+
+	gulp.task('scripts', function() {
+
+		gulp.src([
+				source + 'bower_components/jquery/dist/jquery.js'
+			])
+			.pipe(concat('vendor.js'))
+			.pipe(uglify())
+			.pipe(gulp.dest(destination + 'scripts'))
+			.on('error', errorHandler);
+
+		return gulp.src(source + 'scripts/main.js')
+			.pipe(browserify().on('error', errorHandler))
+			.pipe(uglify())
+			.pipe(gulp.dest(destination + 'scripts'))
+			.on('error', errorHandler);
+	});
+
+	gulp.task('watch', ['styles', 'images', 'scripts'], function() {
+		gulp.watch(source + 'styles/**/*.scss', ['styles']);
+		gulp.watch(source + 'images/**/*', ['images']);
+		gulp.watch(source + 'scripts/**/**.js', ['scripts']);
+	});
+
+	gulp.task('build', ['clean'], function() {
+		gulp.start('styles', 'images', 'scripts');
+	});
+
+	gulp.task('default', ['watch']);
+
+}());
