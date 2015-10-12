74d07c83c080ec203c29ca3a4b31326e14040787
Jamie <jamie@tundra.com.au>
2015-10-12 21:35:23 +1100

    add nav active function

--- src/scripts/components/inner-link.js
+++ src/scripts/components/inner-link.js
@@ -2,27 +2,59 @@ module.exports = function(element) {
 	var element = $(element),
 		links = $('a[href ^= "#"]', element),
 		offset = 60,
-		navLine = $('.nav-line');
+		navLine = $('.nav-line'),
+		timeout;
 
-		links.on('click', function(e) {
-			e.preventDefault();
-			var hash = this.hash,
-				target = $(hash);
+	$('a[href = "#about"]', element).addClass('active');
 
-			$('html, body').stop().animate({
-				'scrollTop': target.offset().top - offset
-			}, 500, 'swing', function() {
-				if(history.pushState) {
-				    history.pushState(null, null, hash);
-				}
-				else {
-				    location.hash = hash;
-				}
-			});
+	links.on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash,
+			target = $(hash);
 
-			if(navLine.hasClass('is-open')) {
-				navLine.removeClass('is-open');
-				$('.navbar-menu', element).slideToggle('hide');
+		links.each(function() {
+			$(this).removeClass('active');
+		});
+
+		$(this).addClass('active');
+
+		$('html, body').stop().animate({
+			'scrollTop': target.offset().top - offset
+		}, 500, 'swing', function() {
+			if(history.pushState) {
+			    history.pushState(null, null, hash);
+			}
+			else {
+			    location.hash = hash;
 			}
 		});
+
+		if(navLine.hasClass('is-open')) {
+			navLine.removeClass('is-open');
+			$('.navbar-menu', element).slideToggle('hide');
+		}
+	});
+
+	$(window).on('scroll', function() {
+		clearTimeout(timeout);
+
+		timeout = setTimeout(function() {
+			var scrollPos = $('html, body').scrollTop();
+
+			links.each(function() {
+				var currLink = $(this),
+					refSection = $(currLink.attr('href')),
+					refTop = refSection.offset().top;
+
+				if ((Math.floor(refTop) - offset) <= scrollPos && (refTop + refSection.outerHeight()) > scrollPos) {
+					links.each(function() {
+						$(this).removeClass('active');
+					});
+					currLink.addClass('active');
+				} else {
+					currLink.removeClass('active');
+				}
+			});
+		}, 100);
+	})
 };
